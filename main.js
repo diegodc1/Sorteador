06232c829dee5de5e6d2ec3838158df587de481e
Diego Alves <diegoalvesdc12@gmail.com>
2022-03-16 14:18:29 -0300

    Third Commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const calculate = {
   minNumber: document.querySelector("input#min-number"),
   maxNumber: document.querySelector("input#max-number"),
   numbers: document.querySelector("#numbers"),
+  result: document.querySelector("#message-result"),
 
   getValues() {
     return {
@@ -14,28 +15,46 @@ const calculate = {
 
   clearResult() {
     calculate.numbers.innerHTML = "";
+    calculate.result.innerHTML = "";
+  },
+
+  clearInputs() {
+    calculate.minNumber.value = "";
+    calculate.maxNumber.value = "";
+    calculate.amountNumbers.value = "";
+    calculate.clearResult();
   },
 
   draw() {
     calculate.clearResult();
     let { minNumber, maxNumber, amountNumbers } = calculate.getValues();
 
+    //add message result
+    let messageResult = document.createElement("p");
+    messageResult.classList.add("message-result");
+    const message = "Os números sorteados foram:";
+    calculate.result.appendChild(messageResult);
+    messageResult.innerHTML = message;
+
     minNumber = Number(minNumber);
     maxNumber = Number(maxNumber);
-    amountNumbers = Number(amountNumbers);
 
+    //add number result
     for (let i = 1; i <= amountNumbers; i++) {
       resultNumber = Math.random() * (maxNumber - minNumber) + minNumber;
 
       let numberResult = document.createElement("p");
       numberResult.classList.add("number-result");
 
-      if (i == 2) {
-        numberResult.innerHTML = "";
-      }
-
       calculate.numbers.appendChild(numberResult);
       numberResult.innerHTML = resultNumber.toFixed(0);
+    }
+
+    //display grid
+    if (amountNumbers > 7) {
+      calculate.numbers.classList.add("grid");
+    } else {
+      calculate.numbers.classList.remove("grid");
     }
   },
 };
